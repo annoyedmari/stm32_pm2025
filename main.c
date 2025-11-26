32bb1f2c36838ee45fbdcd4e51434586209adc83
annoyedmari <annoyedmari@gmail.com>
2025-11-26 17:58:31 +0200

    Practice 4 code uploaded in main.c

--- main.c
+++ main.c
@@ -1,73 +1,115 @@
-#include <stdint.h>
-#include "stm32f10x.h"
-#define LED_PIN (1 << 13)
-#define BUTTON_A_PIN (1 << 0)
-#define BUTTON_C_PIN (1 << 1)
-
-volatile uint32_t blink_delay = 500; // Начальная задержка 500 мс (1 Гц)
-volatile uint32_t min_delay = 15;    // ~15 мс (64 Гц)
-volatile uint32_t max_delay = 64000; // 64000 мс (1/64 Гц)
+#include <stm32f10x.h>
+#define DC_PIN   (1 << 1)   // PA1
+#define RST_PIN  (1 << 2)   // PA2  
+#define CS_PIN   (1 << 3)   // PA3
 
 
-void GPIO_Init(void) {
-    RCC->APB2ENR |= RCC_APB2ENR_IOPAEN | RCC_APB2ENR_IOPCEN;
-    GPIOC->CRH = (GPIOC->CRH & ~(GPIO_CRH_CNF13 | GPIO_CRH_MODE13)) | GPIO_CRH_MODE13_0;
-    GPIOA->CRL = (GPIOA->CRL & ~(GPIO_CRL_CNF0 | GPIO_CRL_MODE0 | GPIO_CRL_CNF1 | GPIO_CRL_MODE1)) 
-                 | (GPIO_CRL_CNF0_1 | GPIO_CRL_CNF1_1);
+void delay(uint32_t ticks) { 
+    while(ticks--) __NOP(); 
+}
+
+void SPI1_Init(void) {
+    RCC->APB2ENR |= RCC_APB2ENR_SPI1EN | RCC_APB2ENR_IOPAEN;
+    GPIOA->CRL = (GPIOA->CRL & ~(0xFFF << 20)) | (0xB8B << 20);
+    SPI1->CR1 = SPI_CR1_MSTR | SPI_CR1_BR_0 | SPI_CR1_SSM | SPI_CR1_SSI | SPI_CR1_SPE;
+}
+
+void SPI1_Write(uint8_t data) {
+    while (!(SPI1->SR & SPI_SR_TXE));
+    SPI1->DR = data;
+    while (SPI1->SR & SPI_SR_BSY);
+}
+
+void SSD1306_Write(uint8_t is_data, uint8_t byte) {
+    GPIOA->BRR = CS_PIN;                    
+    if(is_data)
+        GPIOA->BSRR = DC_PIN;
+    else 
+        GPIOA->BRR = DC_PIN;
     
-    GPIOA->ODR &= ~(BUTTON_A_PIN | BUTTON_C_PIN);
+    SPI1_Write(byte);
+    GPIOA->BSRR = CS_PIN;
+}
+
+void SSD1306_Init(void) {
+    RCC->APB2ENR |= RCC_APB2ENR_IOPAEN;
+    GPIOA->CRL &= ~(GPIO_CRL_CNF1 | GPIO_CRL_MODE1 |
+                    GPIO_CRL_CNF2 | GPIO_CRL_MODE2 |
+                    GPIO_CRL_CNF3 | GPIO_CRL_MODE3);
+    GPIOA->CRL |= (GPIO_CRL_MODE1_0 | GPIO_CRL_MODE2_0 | GPIO_CRL_MODE3_0);
+    // Начальные состояния
+    GPIOA->BSRR = CS_PIN | RST_PIN;
+    // Сброс
+    GPIOA->BRR = RST_PIN; delay(10000);
+    GPIOA->BSRR = RST_PIN; delay(10000);
+    
+    // Дисплей SSD1306
+    const uint8_t init_cmds[] = {
+        0xAE,       // Display OFF
+        0x20, 0x00, // Horizontal addressing mode
+        0x21, 0x00, 0x7F, // Set column address (0-127)
+        0x22, 0x00, 0x07, // Set page address (0-7)
+        0x40,       // Set start line
+        0xA1,       // Segment remap
+        0xC8,       // COM output scan direction
+        0xDA, 0x12, // COM pins hardware configuration
+        0x81, 0x7F, // Set contrast
+        0xA4,       // Entire display ON
+        0xA6,       // Normal display
+        0xD5, 0x80, // Set oscillator frequency
+        0x8D, 0x14, // Enable charge pump
+        0xAF        // Display ON
+    };
+    
+    for(int i = 0; i < sizeof(init_cmds); i++) {
+        SSD1306_Write(0, init_cmds[i]);
+    }
+    delay(10000);
+}
+
+void SSD1306_Clear(void) {
+    for(uint8_t page = 0; page < 8; page++) {
+        SSD1306_Write(0, 0xB0 + page);
+        SSD1306_Write(0, 0x00);
+        SSD1306_Write(0, 0x10);
+        
+        for(uint8_t col = 0; col < 128; col++) {
+            SSD1306_Write(1, 0x00);
+        }
+    }
 }
 
 
-void delay_ms(uint32_t ms) {
-    for(uint32_t i = 0; i < ms * 1000; i++) {
-        __NOP();
-    }
-}
-
-
-void process_buttons(void) {
-    static uint8_t last_button_a = 1;
-    static uint8_t last_button_c = 1;
-    uint32_t debounce_delay = 5000;
-    uint8_t current_button_a = (GPIOA->IDR & BUTTON_A_PIN) ? 1 : 0;
-    uint8_t current_button_c = (GPIOA->IDR & BUTTON_C_PIN) ? 1 : 0;
-    
-    if (!current_button_a && last_button_a) { // button a
-        delay_ms(10);
-        if (!(GPIOA->IDR & BUTTON_A_PIN)) {
-            if (blink_delay > min_delay) {
-                blink_delay /= 2;
+void Chessboard(void) {
+    for(uint8_t page = 0; page < 8; page++) {
+        SSD1306_Write(0, 0xB0 + page);
+        SSD1306_Write(0, 0x00);
+        SSD1306_Write(0, 0x10);
+        
+        for(uint8_t col = 0; col < 128; col++) {
+            uint8_t cell_x = col / 16; // 16 x 16
+            uint8_t cell_y = page / 2;
+            uint8_t pattern;
+            if((cell_x + cell_y) % 2 == 0) {
+                pattern = 0xFF;
+            } else {
+                pattern = 0x00;
             }
-            while (!(GPIOA->IDR & BUTTON_A_PIN)) {
-                __NOP();
-            }
-            delay_ms(10);
+            SSD1306_Write(1, pattern);
         }
     }
-    
-    if (!current_button_c && last_button_c) { // button c
-        delay_ms(10);
-        if (!(GPIOA->IDR & BUTTON_C_PIN)) {
-            if (blink_delay < max_delay) {
-                blink_delay *= 2;
-            }
-            while (!(GPIOA->IDR & BUTTON_C_PIN)) {
-                __NOP();
-            }
-            delay_ms(10);
-        }
-    }
-    
-    last_button_a = current_button_a;
-    last_button_c = current_button_c;
 }
 
 int main(void) {
-    GPIO_Init();
-    while (1) {
-        process_buttons();
-        GPIOC->ODR ^= LED_PIN;
-        delay_ms(blink_delay);
+    SPI1_Init();
+    SSD1306_Init();
+    SSD1306_Clear();
+
+    delay(100000);
+
+    Chessboard();
+
+    while(1) {
+        delay(1000000);
     }
 }
